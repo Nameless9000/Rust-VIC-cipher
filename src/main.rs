67af8a409fd1f54f6ec8e3e749637bac8987ac88
Nameless9000 <dev@name1ess.ga>
2022-10-20 18:48:17 +0100

    starting key generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+/*
+
+wikipedia = helpful
+https://en.wikipedia.org/wiki/VIC_cipher
+
+*/
+
+pub fn generate_key(personal_number: i32, date: &str, phrase: &str, keygroup: i32) -> String {
+    println!("Personal Number: {personal_number}");
+    println!("Date: {date}");
+    println!("Phrase: {phrase}");
+    println!("Keygroup: {keygroup}");
+
+    let out: &str = "lol";
+    return out.to_string();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let personal_number: i32 = 6;
+    let date: &str = "13 Sept 1959";
+    let phrase: &str = "Twas the night before Christmas";
+    let keygroup: i32 = 72401;
+
+    let key: String = generate_key(personal_number, date, phrase, keygroup);
+
+    println!("Key: {key}");
 }
